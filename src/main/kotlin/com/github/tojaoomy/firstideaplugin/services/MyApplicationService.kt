c1445a6064107204c4efcc5d8e3fd7c1f92bc47e
GitHub Action <action@github.com>
2022-01-06 09:55:13 +0000

    Template cleanup

--- src/main/kotlin/com/github/tojaoomy/firstideaplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/tojaoomy/firstideaplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.tojaoomy.firstideaplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.tojaoomy.firstideaplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/tojaoomy/firstideaplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/tojaoomy/firstideaplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.tojaoomy.firstideaplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.tojaoomy.firstideaplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
